7ab468d03f680d71aa40a4315a2f1ffd24fb7617
Simona Cappeddu <simona.cappeddu@gmail.com>
2019-07-20 16:48:42 +0100

    almost done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
